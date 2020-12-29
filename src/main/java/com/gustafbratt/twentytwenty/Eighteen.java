2105e545377687b8cf56d98ca57e33b5e2d1a11e
Gustaf Bratt <Gustaf.Bratt@cybercom.com>
2020-12-29 12:54:51 +0100

    18.2

--- src/main/java/com/gustafbratt/twentytwenty/Eighteen.java
+++ src/main/java/com/gustafbratt/twentytwenty/Eighteen.java
@@ -4,11 +4,19 @@ import com.gustafbratt.utils.AoCUtils;
 
 import java.util.List;
 
-/*
+/* Part 1:
 expressionList = value
-               | value operator expressionList
+               | value [operator value]*
 value = number
       | ( expressionList )
+ */
+/* Part 2:
+product = sum [+ sum]*
+
+sum = value [* value]*
+
+value = number
+      | ( product )
  */
 public class Eighteen {
     static char thisChar;
@@ -46,51 +54,81 @@ public class Eighteen {
 
     public static void main(String[] args) {
         List<String> lines = AoCUtils.fileAsLines("18.txt");
-        long totalSum = 0;
+        long totalSumP1 = 0;
         for(String line : lines) {
-            thisLine = new StringBuilder(line)
-                    .reverse()
-                    .toString()
-                    .replaceAll("\\(", "p")
-                    .replaceAll("\\)", "(")
-                    .replaceAll("p", ")")
-                    .toCharArray();
+            thisLine = line.toCharArray();
             index = 0;
             lookAhead = thisLine[0];
             getNextToken();
-            long i = expressoionList();
+            long i = productP2();
             System.out.println("Result: " + i);
-            totalSum += i;
+            totalSumP1 += i;
         }
-        System.out.println(totalSum);
+        System.out.println(totalSumP1);
     }
 
-    private static long expressoionList() {
-        char start = thisChar;
-        long result = parseValue();
-        if (thisChar == 0 || thisChar == ')')
-            return result;
-        char operator = thisChar;
-        getNextOperator();
-        long val2 = expressoionList();
-        switch (operator) {
-            case '+' -> {
-                System.out.println(result + " + " + val2);
-                result = result + val2;
-            }
-            case '*' -> {
-                System.out.println(result + " * " + val2);
-                result = result * val2;
-            }
-            default -> throw new RuntimeException("Operator: " + operator);
+    //product = sum [+ sum]*
+    private static long productP2() {
+        long product = sumP2();
+        while(thisChar == '*'){
+            getNextToken('*');
+            product *= sumP2();
         }
-        return result;
+        return product;
     }
 
-    private static long parseValue() {
+    //sum = value [* value]*
+    private static long sumP2() {
+        long sum = valueP2();
+        while(thisChar == '+'){
+            getNextToken('+');
+            sum += valueP2();
+        }
+        return sum;
+    }
+
+    //value = number
+    //      | ( expression )
+    private static long valueP2() {
         if(thisChar=='('){
             getNextToken('(');
-            long val = expressoionList();
+            long l = productP2();
+            getNextToken(')');
+            return l;
+        }else{
+            long val = Character.getNumericValue(thisChar);
+            getNextNumber();
+            return val;
+        }
+    }
+
+    private static long parseExpressoionListP1() {
+        char start = thisChar;
+        long result = parseValueP1();
+        while(true) {
+            if (thisChar == 0 || thisChar == ')')
+                return result;
+            char operator = thisChar;
+            getNextOperator();
+            long val2 = parseValueP1();
+            switch (operator) {
+                case '+' -> {
+                    System.out.println(result + " + " + val2);
+                    result = result + val2;
+                }
+                case '*' -> {
+                    System.out.println(result + " * " + val2);
+                    result = result * val2;
+                }
+                default -> throw new RuntimeException("Operator: " + operator);
+            }
+        }
+    }
+
+    private static long parseValueP1() {
+        if(thisChar=='('){
+            getNextToken('(');
+            long val = parseExpressoionListP1();
             getNextToken(')');
             return val;
         }else{
